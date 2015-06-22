2a2903490188b30d858d7270737b5871a17c8e43
Kaleb Walton <kalebwalton@gmail.com>
2015-06-22 13:27:15 -0400

    updates

--- recipes/tmux_settings.rb
+++ recipes/tmux_settings.rb
@@ -1,7 +1,5 @@
 include_recipe "sprout-base::user_owns_usr_local"
 
-homebrew_cask "tmux"
-
 template "#{node['sprout']['home']}/.tmux.conf" do
   source "tmux_settings-.tmux.conf.erb"
   owner node['sprout']['user']
