679805c2dd4077dee1ecd87dc1198fd903e1eb4b
Kaleb Walton <kalebwalton@gmail.com>
2015-06-22 13:24:40 -0400

    added metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'sprout-wrap'
+maintainer       'Kaleb Walton'
+maintainer_email 'kalebwalton@gmail.com'
+license          'MIT'
+description      'Kalebs sprout wrap'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+supports         'mac_os_x'
+depends          'osx'
